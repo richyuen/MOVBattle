b1fa5625194af24ed692ead4929d0cb27bb83caa
richyuen <80013917+richyuen@users.noreply.github.com>
2026-04-03 08:43:03 -0400

    Increase default team budgets

--- web/src/core/battleConfig.ts
+++ web/src/core/battleConfig.ts
@@ -1,6 +1,6 @@
 export const BATTLE_CONFIG = {
-  teamABudget: 100000,
-  teamBBudget: 100000,
+  teamABudget: 1000000,
+  teamBBudget: 1000000,
   maxUnitsPerTeam: 100,
   countdownSeconds: 1.5,
   optionalTimeLimitSeconds: 120,
